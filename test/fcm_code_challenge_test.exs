242b090bb94e43d30c74bde045f0cfb0ee500037
Bardeson Lucky <flashup4all@gmail.com>
2022-09-21 11:53:04 +0100

    added test case

--- test/fcm_code_challenge_test.exs
+++ test/fcm_code_challenge_test.exs
@@ -2,12 +2,9 @@ defmodule FcmCodeChallengeTest do
   use ExUnit.Case
   doctest FcmCodeChallenge
 
-  # test "greets the world" do
-  #   # assert FcmCodeChallenge.hello() == :world
-  # end
   test "test that file exist" do
     src = "assets/input.txt"
     assert File.exists?(src)
-   assert FcmCodeChallenge.process_input(src)
+    assert FcmCodeChallenge.process_input(src)
   end
 end
